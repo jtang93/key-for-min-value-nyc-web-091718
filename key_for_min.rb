c9f5c82e23220237a12b4a5639676245a3bc5d45
jtang93 <tang.jonathan93@gmail.com>
2018-09-14 10:56:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -19,6 +19,7 @@ def key_for_min_value(name_hash)
       i += 1
     end
   end
+  puts "#{k}, #{v}, #{smallest}, #{k[v.index(smallest)]}"
   k[v.index(smallest)]
-puts "#{k}, #{v}, #{smallest}, #{k[v.index(smallest)]}"
+
 end
